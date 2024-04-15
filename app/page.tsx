9521ab5e68e5eb9eea17eb52af26c49aba87181c
justjazz903 <justjazz903@gmail.com>
2024-04-15 10:04:42 +0800

    Mon, Apr 15, 2024 10:04:42 AM

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Button_1 from '@/components/button_1/Button';
 import Button_2 from '@/components/button_2/Button';
+import Button_3 from '@/components/button_3/Button';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center my-8 space-y-16">
       <Button_1 />
       <Button_2 />
+      <Button_3 />
     </div>
   );
 }
