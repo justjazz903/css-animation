cdf474cde27a4da70508630444d74e8e3f887cba
justjazz903 <justjazz903@gmail.com>
2024-04-18 17:14:37 +0800

    Thu, Apr 18, 2024  5:14:37 PM

--- components/button_10/Button.tsx
+++ components/button_10/Button.tsx
@@ -2,7 +2,7 @@ import styles from './Button.module.css'
 
 export default function Button({href, text}: {href: string, text: string}) {
   return (
-    <div className='flex overflow-hidden'>
+    <div className='flex'>
       <a href={href} className={styles.custom_button}>
         {text}
       </a>
